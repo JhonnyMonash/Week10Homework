27e33161d4dcb55fc26fd88cfecf61fce0e8c99c
john Zhang <jzha0350@student.monash.edu>
2024-10-03 15:04:59 +1000

    HWFinal

--- js/something2.js
+++ js/something2.js
@@ -1,2 +1,30 @@
 var vg_2 = "js/station_patronage.vg.json";
-vegaEmbed("#vg_2", vg_2, { "actions": false }).then(function (result) { }).catch(console.error);
+vegaEmbed("#vg_2", vg_2, { "actions": false }).then(function (result) {
+    var view = result.view;
+    var lines = [
+        'Alamein',
+        'Belgrave',
+        'Craigieburn',
+        'Cranbourne',
+        'Frankston',
+        'Glen Waverley',
+        'Hurstbridge',
+        'Lilydale',
+        'Mernda',
+        'Pakenham',
+        'Sandringham',
+        'Sunbury',
+        'Upfield',
+        'Werribee',
+        'Williamstown'
+    ];
+
+    // Bind checkboxes to Vega-Lite parameters
+    lines.forEach(function (line) {
+        var paramName = 'line_' + line.replace(/\s+/g, '_');
+        var checkbox = document.getElementById(paramName);
+        checkbox.addEventListener('change', function () {
+            view.signal(paramName, checkbox.checked).run();
+        });
+    });
+ }).catch(console.error);
